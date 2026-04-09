1227ba12b5fb8513be20cacccff47d2edac396b6
Vageesh <vageeshhiremath962@gmail.com>
2026-04-09 23:57:44 +0530

    Delete logger.py

--- logger.py
+++
@@ -1,9 +0,0 @@
-import json
-import time
-
-LOG_FILE = "events.json"
-
-def log_event(event):
-    event["timestamp"] = time.strftime("%Y-%m-%d %H:%M:%S")
-    with open(LOG_FILE, "a") as f:
-        f.write(json.dumps(event) + "\n")
